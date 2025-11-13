d09d4d8cef3fd9487e68c0ace6cc998b40b54d73
ydshiroma <ydshiroma@gmail.com>
2025-11-13 08:55:08 -0500

    early work in evaluateBoard

--- js/main.js
+++ js/main.js
@@ -42,98 +42,106 @@ timer = null;
 var weights = { p: 100, n: 300, b: 300, r: 500, q: 900, k: 2000, k_e: 2000 };
 
 // var weights = { p: 100, n: 280, b: 320, r: 479, q: 929, k: 60000, k_e: 60000 };
-var pst_w = {
-  p: [
-    [100, 100, 100, 100, 105, 100, 100, 100],
-    [78, 83, 86, 73, 102, 82, 85, 90],
-    [7, 29, 21, 44, 40, 31, 44, 7],
-    [-17, 16, -2, 15, 14, 0, 15, -13],
-    [-26, 3, 10, 9, 6, 1, 0, -23],
-    [-22, 9, 5, -11, -10, -2, 3, -19],
-    [-31, 8, -7, -37, -36, -14, 3, -31],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-  ],
-  n: [
-    [-66, -53, -75, -75, -10, -55, -58, -70],
-    [-3, -6, 100, -36, 4, 62, -4, -14],
-    [10, 67, 1, 74, 73, 27, 62, -2],
-    [24, 24, 45, 37, 33, 41, 25, 17],
-    [-1, 5, 31, 21, 22, 35, 2, 0],
-    [-18, 10, 13, 22, 18, 15, 11, -14],
-    [-23, -15, 2, 0, 2, 0, -23, -20],
-    [-74, -23, -26, -24, -19, -35, -22, -69],
-  ],
-  b: [
-    [-59, -78, -82, -76, -23, -107, -37, -50],
-    [-11, 20, 35, -42, -39, 31, 2, -22],
-    [-9, 39, -32, 41, 52, -10, 28, -14],
-    [25, 17, 20, 34, 26, 25, 15, 10],
-    [13, 10, 17, 23, 17, 16, 0, 7],
-    [14, 25, 24, 15, 8, 25, 20, 15],
-    [19, 20, 11, 6, 7, 6, 20, 16],
-    [-7, 2, -15, -12, -14, -15, -10, -10],
-  ],
-  r: [
-    [35, 29, 33, 4, 37, 33, 56, 50],
-    [55, 29, 56, 67, 55, 62, 34, 60],
-    [19, 35, 28, 33, 45, 27, 25, 15],
-    [0, 5, 16, 13, 18, -4, -9, -6],
-    [-28, -35, -16, -21, -13, -29, -46, -30],
-    [-42, -28, -42, -25, -25, -35, -26, -46],
-    [-53, -38, -31, -26, -29, -43, -44, -53],
-    [-30, -24, -18, 5, -2, -18, -31, -32],
-  ],
-  q: [
-    [6, 1, -8, -104, 69, 24, 88, 26],
-    [14, 32, 60, -10, 20, 76, 57, 24],
-    [-2, 43, 32, 60, 72, 63, 43, 2],
-    [1, -16, 22, 17, 25, 20, -13, -6],
-    [-14, -15, -2, -5, -1, -10, -20, -22],
-    [-30, -6, -13, -11, -16, -11, -16, -27],
-    [-36, -18, 0, -19, -15, -15, -21, -38],
-    [-39, -30, -31, -13, -31, -36, -34, -42],
-  ],
-  k: [
-    [4, 54, 47, -99, -99, 60, 83, -62],
-    [-32, 10, 55, 56, 56, 55, 10, 3],
-    [-62, 12, -57, 44, -67, 28, 37, -31],
-    [-55, 50, 11, -4, -19, 13, 0, -49],
-    [-55, -43, -52, -28, -51, -47, -8, -50],
-    [-47, -42, -43, -79, -64, -32, -29, -32],
-    [-4, 3, -14, -50, -57, -18, 13, 4],
-    [17, 30, -3, -14, 6, -1, 40, 18],
-  ],
+// var pst_w = {
+//   p: [
+//     [100, 100, 100, 100, 105, 100, 100, 100],
+//     [78, 83, 86, 73, 102, 82, 85, 90],
+//     [7, 29, 21, 44, 40, 31, 44, 7],
+//     [-17, 16, -2, 15, 14, 0, 15, -13],
+//     [-26, 3, 10, 9, 6, 1, 0, -23],
+//     [-22, 9, 5, -11, -10, -2, 3, -19],
+//     [-31, 8, -7, -37, -36, -14, 3, -31],
+//     [0, 0, 0, 0, 0, 0, 0, 0],
+//   ],
+//   n: [
+//     [-66, -53, -75, -75, -10, -55, -58, -70],
+//     [-3, -6, 100, -36, 4, 62, -4, -14],
+//     [10, 67, 1, 74, 73, 27, 62, -2],
+//     [24, 24, 45, 37, 33, 41, 25, 17],
+//     [-1, 5, 31, 21, 22, 35, 2, 0],
+//     [-18, 10, 13, 22, 18, 15, 11, -14],
+//     [-23, -15, 2, 0, 2, 0, -23, -20],
+//     [-74, -23, -26, -24, -19, -35, -22, -69],
+//   ],
+//   b: [
+//     [-59, -78, -82, -76, -23, -107, -37, -50],
+//     [-11, 20, 35, -42, -39, 31, 2, -22],
+//     [-9, 39, -32, 41, 52, -10, 28, -14],
+//     [25, 17, 20, 34, 26, 25, 15, 10],
+//     [13, 10, 17, 23, 17, 16, 0, 7],
+//     [14, 25, 24, 15, 8, 25, 20, 15],
+//     [19, 20, 11, 6, 7, 6, 20, 16],
+//     [-7, 2, -15, -12, -14, -15, -10, -10],
+//   ],
+//   r: [
+//     [35, 29, 33, 4, 37, 33, 56, 50],
+//     [55, 29, 56, 67, 55, 62, 34, 60],
+//     [19, 35, 28, 33, 45, 27, 25, 15],
+//     [0, 5, 16, 13, 18, -4, -9, -6],
+//     [-28, -35, -16, -21, -13, -29, -46, -30],
+//     [-42, -28, -42, -25, -25, -35, -26, -46],
+//     [-53, -38, -31, -26, -29, -43, -44, -53],
+//     [-30, -24, -18, 5, -2, -18, -31, -32],
+//   ],
+//   q: [
+//     [6, 1, -8, -104, 69, 24, 88, 26],
+//     [14, 32, 60, -10, 20, 76, 57, 24],
+//     [-2, 43, 32, 60, 72, 63, 43, 2],
+//     [1, -16, 22, 17, 25, 20, -13, -6],
+//     [-14, -15, -2, -5, -1, -10, -20, -22],
+//     [-30, -6, -13, -11, -16, -11, -16, -27],
+//     [-36, -18, 0, -19, -15, -15, -21, -38],
+//     [-39, -30, -31, -13, -31, -36, -34, -42],
+//   ],
+//   k: [
+//     [4, 54, 47, -99, -99, 60, 83, -62],
+//     [-32, 10, 55, 56, 56, 55, 10, 3],
+//     [-62, 12, -57, 44, -67, 28, 37, -31],
+//     [-55, 50, 11, -4, -19, 13, 0, -49],
+//     [-55, -43, -52, -28, -51, -47, -8, -50],
+//     [-47, -42, -43, -79, -64, -32, -29, -32],
+//     [-4, 3, -14, -50, -57, -18, 13, 4],
+//     [17, 30, -3, -14, 6, -1, 40, 18],
+//   ],
 
-  // Endgame King Table
-  k_e: [
-    [-50, -40, -30, -20, -20, -30, -40, -50],
-    [-30, -20, -10, 0, 0, -10, -20, -30],
-    [-30, -10, 20, 30, 30, 20, -10, -30],
-    [-30, -10, 30, 40, 40, 30, -10, -30],
-    [-30, -10, 30, 40, 40, 30, -10, -30],
-    [-30, -10, 20, 30, 30, 20, -10, -30],
-    [-30, -30, 0, 0, 0, 0, -30, -30],
-    [-50, -30, -30, -30, -30, -30, -30, -50],
-  ],
-};
-var pst_b = {
-  p: pst_w['p'].slice().reverse(),
-  n: pst_w['n'].slice().reverse(),
-  b: pst_w['b'].slice().reverse(),
-  r: pst_w['r'].slice().reverse(),
-  q: pst_w['q'].slice().reverse(),
-  k: pst_w['k'].slice().reverse(),
-  k_e: pst_w['k_e'].slice().reverse(),
-};
+//   // Endgame King Table
+//   k_e: [
+//     [-50, -40, -30, -20, -20, -30, -40, -50],
+//     [-30, -20, -10, 0, 0, -10, -20, -30],
+//     [-30, -10, 20, 30, 30, 20, -10, -30],
+//     [-30, -10, 30, 40, 40, 30, -10, -30],
+//     [-30, -10, 30, 40, 40, 30, -10, -30],
+//     [-30, -10, 20, 30, 30, 20, -10, -30],
+//     [-30, -30, 0, 0, 0, 0, -30, -30],
+//     [-50, -30, -30, -30, -30, -30, -30, -50],
+//   ],
+// };
+// var pst_b = {
+//   p: pst_w['p'].slice().reverse(),
+//   n: pst_w['n'].slice().reverse(),
+//   b: pst_w['b'].slice().reverse(),
+//   r: pst_w['r'].slice().reverse(),
+//   q: pst_w['q'].slice().reverse(),
+//   k: pst_w['k'].slice().reverse(),
+//   k_e: pst_w['k_e'].slice().reverse(),
+// };
 
-var pstOpponent = { w: pst_b, b: pst_w };
-var pstSelf = { w: pst_w, b: pst_b };
+// var pstOpponent = { w: pst_b, b: pst_w };
+// var pstSelf = { w: pst_w, b: pst_b };
 
 /*
  * Evaluates the board at this point in time,
  * using the material weights and piece square tables.
  */
+//TODO: instead of using prevSum, just add up value of all pieces on the board?
 function evaluateBoard(game, move, prevSum, color) {
+  point_total = 0
+
+  console.log("game: " + JSON.stringify(game));
+  console.log("move: " + JSON.stringify(move));
+  console.log("prevSum: " + prevSum);
+  console.log("color: " + color);
+  //console.log("get pieces: " + game.getPieces({ type: 'n', color: 'w' }));
 
   if (game.in_checkmate()) {
 
@@ -152,73 +160,38 @@ function evaluateBoard(game, move, prevSum, color) {
     return 0;
   }
 
-  // TODO: change this? get rid of it? should be covered by mobility calculation
-  // if (game.in_check()) {
-  //   // Opponent is in check (good for us)
-  //   if (move.color === color) {
-  //     prevSum += 50;
-  //   }
-  //   // Our king's in check (bad for us)
-  //   else {
-  //     prevSum -= 50;
-  //   }
-  // }
+  console.log("squares: " + game["SQUARES"])
+  for (let i = 0; i < game["SQUARES"].length; i++) {
+    //console.log(game["SQUARES"][i]);
+    //console.log(game["SQUARES"][i].piece);
+    //console.log(game.get(game["SQUARES"][i]));
 
-  // var from = [
-  //   8 - parseInt(move.from[1]),
-  //   move.from.charCodeAt(0) - 'a'.charCodeAt(0),
-  // ];
-  // var to = [
-  //   8 - parseInt(move.to[1]),
-  //   move.to.charCodeAt(0) - 'a'.charCodeAt(0),
-  // ];
-
-  //TODO: get rid of this?
-  // Change endgame behavior for kings
-  // if (prevSum < -1500) {
-  //   if (move.piece === 'k') {
-  //     move.piece = 'k_e';
-  //   }
-    // Kings can never be captured
-    // else if (move.captured === 'k') {
-    //   move.captured = 'k_e';
-    // }
-  // }
-
-  if ('captured' in move) {
-    // Opponent piece was captured (good for us)
-    if (move.color === color) {
-      prevSum +=
-        weights[move.captured];
+    const piece = game.get(game["SQUARES"][i]);
+    let piece_value;
+    if (piece) {
+      if (piece["color"] == color) {
+        console.log("piece is " + color);
+        piece_value = weights[piece["type"]];        
+      } else {
+        console.log("piece is " + color);
+        piece_value = -weights[piece["type"]];
+      }
+      console.log(piece_value);
+      point_total += piece_value;
     }
-    // Our piece was captured (bad for us)
-    else {
-      prevSum -=
-        weights[move.captured];
-    }
+    
   }
 
-  if (move.flags.includes('p')) {
-    // NOTE: promote to queen for simplicity
-    move.promotion = 'q';
-
-    // Our piece was promoted (good for us)
-    if (move.color === color) {
-      prevSum -=
-        weights[move.piece];
-      prevSum +=
-        weights[move.promotion];
-    }
-    // Opponent piece was promoted (bad for us)
-    else {
-      prevSum +=
-        weights[move.piece];
-      prevSum -=
-        weights[move.promotion];
-    }
-  }
-
-  return prevSum;
+  // TODO: calculate number of possible moves for each piece and add to prevSum
+  // 8 possible moves = 100 (equivalent to one pawn)
+  //TODO: need to do this for both colors? I'm confused. or am I just adding the value of one side's material here?
+  const value_one_move = 100 / 8;
+  const available_moves = game.moves().length;
+  console.log("available moves: " + available_moves)
+  const num_to_add = value_one_move * available_moves;
+  point_total += num_to_add;
+  console.log(point_total);
+  return point_total;
 }
 
 /*
@@ -239,6 +212,9 @@ function evaluateBoard(game, move, prevSum, color) {
  *  the best move at the root of the current subtree.
  */
 function minimax(game, depth, alpha, beta, isMaximizingPlayer, sum, color) {
+  // TODO: how to hard-code depth? is it 1 move = white and black both play or 1 move = either white or black plays?
+  // depth = 2
+
   positionCount++;
   var children = game.ugly_moves({ verbose: true });
 
